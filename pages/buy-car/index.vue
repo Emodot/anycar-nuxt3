732f867ce1c291bc677fca22a006226a78b4aa55
David Emaye <emayeodavid@gmail.com>
2023-10-25 18:09:23 +0100

    migrating buy-car page

--- pages/buy-car/index.vue
+++ pages/buy-car/index.vue
@@ -2,12 +2,10 @@
   <div class="main_ctn">
     <div class="inner">
       <div class="top_section">
-        <h2 class="title">
-          Search for Cars on AnyCar
-        </h2>
+        <h2 class="title">Search for Cars on AnyCar</h2>
         <SearchInput />
         <div class="content">
-          <FilterCars
+          <!-- <FilterCars
             :data="cars"
             :loading="getCarsLoading"
             @changePage="changePage"
@@ -16,129 +14,115 @@
             @filterPrice="filterPrice"
             @filterMileage="filterMileage"
             @filterType="filterType"
-          />
+          /> -->
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  data () {
-    return {
-      cars: {},
-      currentPage: 1,
-      make: '',
-      year: '',
-      maxPrice: '',
-      maxMileage: '',
-      type: '',
-      getCarsLoading: false
-    }
-  },
-  created () {
-    this.getCars(
-      this.currentPage,
-      this.make,
-      this.year,
-      this.maxPrice,
-      this.maxMileage,
-      this.type
-    )
-  },
-  methods: {
-    changePage (val) {
-      this.getCars(
-        val,
-        this.make,
-        this.year,
-        this.maxPrice,
-        this.maxMileage,
-        this.type
-      )
-    },
-    filterMake (val) {
-      this.getCars(
-        this.currentPage,
-        val,
-        this.year,
-        this.maxPrice,
-        this.maxMileage,
-        this.type
-      )
-    },
-    filterYear (val) {
-      this.getCars(
-        this.currentPage,
-        this.make,
-        val,
-        this.maxPrice,
-        this.maxMileage,
-        this.type
-      )
-    },
-    filterPrice (val) {
-      this.getCars(
-        this.currentPage,
-        this.make,
-        this.year,
-        val,
-        this.maxMileage,
-        this.type
-      )
-    },
-    filterMileage (val) {
-      this.getCars(
-        this.currentPage,
-        this.make,
-        this.year,
-        this.maxPrice,
-        val,
-        this.type
-      )
-    },
-    filterType (val) {
-      this.getCars(
-        this.currentPage,
-        this.make,
-        this.year,
-        this.maxPrice,
-        this.maxMileage,
-        val
-      )
-    },
-    getCars (
-      currentPage,
-      make,
-      year,
-      maxPrice,
-      maxMileage,
-      type
-    ) {
-      this.getCarsLoading = true
-      // console.log(type)
-      this.$axios.$get(`api/sell?pageNumber=${currentPage}&pageSize=9&make=${make}&year=${year}&maxPrice=${maxPrice}&maxMileage=${maxMileage}&type=${type}`)
-        .then((response) => {
-          console.log(response)
-          this.cars = response
-        })
-        .catch((_err) => {
-          const errorMsg = _err?.response?.data?.error || _err?.message
-          const feedback = {
-            content:
-              errorMsg || 'Oops, something went wrong, please try again later',
-            state: 'error'
-          }
-          console.log(feedback)
-          this.$toaster.showToast(feedback)
-        })
-        .finally(() => {
-          this.getCarsLoading = false
-        })
-    }
-  }
-}
+<script setup>
+import axios from "axios";
+const config = useRuntimeConfig();
+const baseUrl = config.public.BASE_URL;
+
+const getCarsLoading = ref(false);
+const cars = ref({});
+const currentPage = ref(1);
+const make = ref("");
+const year = ref("");
+const maxPrice = ref("");
+const maxMileage = ref("");
+const type = ref("");
+
+onMounted(() => {
+  getCars(
+    this.currentPage,
+    this.make,
+    this.year,
+    this.maxPrice,
+    this.maxMileage,
+    this.type
+  );
+});
+
+const changePage = (val) => {
+  getCars(val, this.make, this.year, this.maxPrice, this.maxMileage, this.type);
+};
+const filterMake = (val) => {
+  getCars(
+    this.currentPage,
+    val,
+    this.year,
+    this.maxPrice,
+    this.maxMileage,
+    this.type
+  );
+};
+const filterYear = (val) => {
+  getCars(
+    this.currentPage,
+    this.make,
+    val,
+    this.maxPrice,
+    this.maxMileage,
+    this.type
+  );
+};
+const filterPrice = (val) => {
+  getCars(
+    this.currentPage,
+    this.make,
+    this.year,
+    val,
+    this.maxMileage,
+    this.type
+  );
+};
+const filterMileage = (val) => {
+  getCars(
+    this.currentPage,
+    this.make,
+    this.year,
+    this.maxPrice,
+    val,
+    this.type
+  );
+};
+const filterType = (val) => {
+  getCars(
+    this.currentPage,
+    this.make,
+    this.year,
+    this.maxPrice,
+    this.maxMileage,
+    val
+  );
+};
+const getCars = (currentPage, make, year, maxPrice, maxMileage, type) => {
+  getCarsLoading.value = true;
+  // console.log(type)
+  const path = `api/sell?pageNumber=${currentPage}&pageSize=9&make=${make}&year=${year}&maxPrice=${maxPrice}&maxMileage=${maxMileage}&type=${type}`;
+  axios
+    .get(`${baseUrl}${path}`)
+    .then((response) => {
+      console.log(response);
+      cars.value = response;
+    })
+    .catch((_err) => {
+      const errorMsg = _err?.response?.data?.error || _err?.message;
+      const feedback = {
+        content:
+          errorMsg || "Oops, something went wrong, please try again later",
+        state: "error",
+      };
+      console.log(feedback);
+      // this.$toaster.showToast(feedback);
+    })
+    .finally(() => {
+      this.getCarsLoading = false;
+    });
+};
 </script>
 
 <style scoped>
