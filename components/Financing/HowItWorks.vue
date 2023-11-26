df665937eb9c3f8ee689e22e69f829428294eabb
David Emaye <emayeodavid@gmail.com>
2023-11-27 00:25:51 +0100

    fixed isuess how it works section

--- components/Financing/HowItWorks.vue
+++ components/Financing/HowItWorks.vue
@@ -11,6 +11,28 @@
           <div class="list_icon">
             <span class="material-icons-outlined"> check_circle </span>
           </div>
+          <div class="list_content">
+            <p class="list_title">Identification</p>
+            <p class="list_text">The identification phase mandates the submission of your personal information, crucial for the processing of your loan application</p>
+          </div>
+        </div>
+        <div class="list_ctn">
+          <div class="list_icon">
+            <span class="material-icons-outlined"> check_circle </span>
+          </div>
+          <div class="list_content">
+            <p class="list_title">Prequalification</p>
+            <p class="list_text">To initiate the process, kindly fill out a brief form with your business information and BVN (for both salary earners and business owners). This data will be utilized to determine your eligibility amount.</p>
+          </div>
+        </div>
+        <div class="list_ctn">
+          <div class="list_icon">
+            <span class="material-icons-outlined"> check_circle </span>
+          </div>
+          <div class="list_content">
+            <p class="list_title">Browse Cars</p>
+            <p class="list_text">Afterward, explore a customized page displaying the range of cars you qualify for financing, tailored to your pre-qualification status.</p>
+          </div>
         </div>
         <button class="global_btn_2">
           <p>Check Prequalification</p>
@@ -52,11 +74,12 @@ p {
 }
 
 h1 {
-  font-weight: 900;
+  font-weight: 700;
 }
 
 .sub_title {
-  margin-top: 2rem;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
   line-height: 20px;
   width: 80%;
 }
@@ -73,14 +96,30 @@ h1 {
   font-weight: 800;
 }
 
+.list_icon {
+  margin-right: 10px;
+}
+
 .material-icons-outlined {
-  color: var(--primary-color);
+  color: #FF9900;
   font-weight: 200;
-  font-size: 20px;
+  font-size: 22px;
 }
 
 .list_ctn {
   display: flex;
+  margin-bottom: 1rem;
+}
+
+.list_title {
+  font-weight: 800;
+  font-size: 16px;
+}
+
+.list_text {
+  font-size: 14px;
+  margin-top: 7px;
+  color: #ffffff99;
 }
 
 .rhs {
